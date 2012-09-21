6208883626e2a9839ca75430b1a7ce83cdb1ad7c
David Yeung <quxbaz@gmail.com>
2012-09-21 02:13:45 -0400

    Migration over to flask framework.

---
+++ static/js/menu.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    var $groups = $('#menu-groups li');
+
+    $('#menu-items ul').hide();
+
+    $groups.on('click', function(e) {
+        $('#menu-items ul').hide();
+        $('#' + $(this).attr('for')).show();
+        $groups.removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
+    $($groups).hover(function(e) {
+        $(this).addClass('hover');
+    },function(e) {
+        $(this).removeClass('hover');
+    });
+
+    $('#menu-groups [for="menu-appetizers"]').click();
+
+});
